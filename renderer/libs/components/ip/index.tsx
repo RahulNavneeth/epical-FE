7dc0df7dbb84de18e147a5e8b3b7dd8e06d74502
RahulNavneeth <rahulmnavneeth@gmail.com>
2024-01-31 11:21:04 +0530

    ipv4

--- renderer/libs/components/ip/index.tsx
+++ renderer/libs/components/ip/index.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useIPStore } from "../../store"
 import axios from "axios";
 
 const IP = () => {
     const [ip, setIP] = useState("");
     const [check, setCheck] = useState<boolean>(false);
+    const [error, setError] = useState<string>("");
     const setIPStore = useIPStore((state) => state.setIP);
 
     const handleIP = async () => {
@@ -12,7 +13,7 @@ const IP = () => {
 
         const regex = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
         if (!regex.test(ip)) {
-            alert("Invalid IP Address");
+            setError("Invalid IP Address");
             setCheck(false);
             return;
         }
@@ -21,7 +22,7 @@ const IP = () => {
         console.log(ipR);
 
         const axiosPromise = axios.get(ipR);
-        const timeoutPromise = new Promise((resolve, reject) => {
+        const timeoutPromise = new Promise((_, reject) => {
             setTimeout(() => {
                 reject(new Error("Request timed out"));
             }, 3000);
@@ -31,27 +32,37 @@ const IP = () => {
             const response = await Promise.race([axiosPromise, timeoutPromise]);
             //@ts-ignore
             const { data } = response;
+            { !!error && <div className="text-red-500 font-semibold">{error}</div> }
 
             if (!data.success) {
-                alert("Invalid IP Address");
+                setError("Invalid IP Address");
                 return;
             }
 
             setIPStore(ipR);
         } catch (e) {
             console.log(ipR, "CATCH");
-            alert("Invalid IP Address");
+            setError("Invalid IP Address");
         } finally {
             setCheck(false);
         }
     }
+    useEffect(() => {
+        if (!!error) {
+            setTimeout(() => {
+                setError("");
+            }, 3000);
+        }
+    }, [error])
+
     return (
         <div className="w-screen h-screen flex flex-col items-center justify-evenly font-primary">
             <div className="text-3xl font-black"> FOR ADMIN USE ONLY!! </div>
             <div className="w-full flex flex-col items-center justify-center">
+                {!!error && <div className="text-red-500 font-semibold">{error}</div>}
                 <div className="w-[30%]">
                     <div>Enter IP:</div>
-                    <input className="border-black border-2 w-full outline-none px-2 py-4" onChange={(i) => setIP(i.target.value)} type="text" />
+                    <input className="bg-gray-100 border-black border-2 w-full outline-none px-2 py-4" onChange={(i) => setIP(i.target.value)} type="text" />
                 </div>
                 <button disabled={check} onClick={handleIP} className="disabled:bg-green-200 disabled:text-gray-500 bg-green-500 mt-4 outline-none px-4 py-2 font-semibold w-[200px] rounded-md text-center">{!check ? "CONNECT" : <>CHECKING</>}</button>
             </div>
